c37369a9e7c446e8722dc085be5fb1bf917fd17c
Hassieb <hassieb.n.h@gmail.com>
2025-08-12 22:31:59 -0700

    Completed Sprint 3: Dynamic Creation

--- lib/canvas_screen.dart
+++ lib/canvas_screen.dart
@@ -10,33 +10,62 @@ class CanvasScreen extends StatefulWidget {
 }
 
 class _CanvasScreenState extends State<CanvasScreen> {
-  final CanvasNode _node = CanvasNode(
-    id: "some_id",
-    content: "The unexamined life is not worth living.",
-    x: 0.0,
-    y: 0.0,
-    nodeType: NodeType.thought,
-  );
+  final List<CanvasNode> _nodes = [
+    CanvasNode(
+      id: "some_id",
+      content: "The unexamined life is not worth living.",
+      x: 0.0,
+      y: 0.0,
+      nodeType: NodeType.thought,
+    ),
+    CanvasNode(
+      id: "some_id_2",
+      content: "This is a content example",
+      x: 5,
+      y: 5,
+      nodeType: NodeType.thought,
+    ),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Canvas Prototype")),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _addNode,
+        child: Icon(Icons.add),
+      ),
       body: InteractiveViewer(
         constrained: false,
         boundaryMargin: EdgeInsets.all(double.infinity),
         child: Stack(
           children: [
             Container(width: 2000, height: 2000, color: Color(0xFFF0F0F0)),
-            Positioned(
-              left: _node.x,
-              top: _node.y,
-              child: NodeWidget(node: _node),
-            ),
+            ..._nodes.map((node) {
+              return Positioned(
+                left: node.x,
+                top: node.y,
+                child: NodeWidget(node: node),
+              );
+            }),
           ],
         ),
       ),
       backgroundColor: Color.fromARGB(255, 0, 0, 0),
     );
+  }
+
+  void _addNode() {
+    setState(() {
+      _nodes.add(
+        CanvasNode(
+          id: DateTime.now().toIso8601String(),
+          content: "New Thought",
+          x: 0,
+          y: 0,
+          nodeType: NodeType.thought,
+        ),
+      );
+    });
   }
 }
